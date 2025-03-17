dd709055973c8cffea0a2a2ef3f7d0bb6e23b39d
Faust Magno <neurochange@proton.me>
2025-03-17 15:36:18 +0000

    Create starship.sh

---
+++ files/scripts/starship.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
+curl -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
+tar -xzf /tmp/starship.tar.gz -C /tmp
+install -c -m 0755 /tmp/starship /usr/bin
