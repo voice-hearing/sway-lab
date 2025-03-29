ece0b39f3a40b75936b8d07994c942be1c9b4725
Faust Magno <neurochange@proton.me>
2025-03-29 20:31:03 +0000

    Update starship.sh

--- files/scripts/starship.sh
+++ files/scripts/starship.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 
 # Tell build process to exit if there are any errors.
-set -oue pipefail
+set -ouex pipefail
 
+# github direct installs
+/ctx/build_files/github-release-install.sh twpayne/chezmoi x86_64
+
+# Starship Shell Prompt
 curl -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
 tar -xzf /tmp/starship.tar.gz -C /tmp
 install -c -m 0755 /tmp/starship /usr/bin
+# shellcheck disable=SC2016
+echo 'eval "$(starship init bash)"' >> /etc/bashrc
+
+# Bash Prexec
+curl -Lo /usr/share/bash-prexec https://raw.githubusercontent.com/rcaloras/bash-preexec/master/bash-preexec.sh
+
+# Consolidate Just Files
+find /tmp/just -iname '*.just' -exec printf "\n\n" \; -exec cat {} \; >> /usr/share/ublue-os/just/60-custom.just
+# ensure any just recipes needing fedora version are updated
+sed -i "s/FEDORA_MAJOR_VERSION/${FEDORA_MAJOR_VERSION}/" /usr/share/ublue-os/just/60-custom.just
