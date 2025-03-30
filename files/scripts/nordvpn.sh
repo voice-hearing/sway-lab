03298b87c3d0387a84fc58e942fba26dc144768f
Faust Magno <neurochange@proton.me>
2025-03-30 18:59:27 +0100

    Update nordvpn.sh

--- files/scripts/nordvpn.sh
+++ files/scripts/nordvpn.sh
@@ -3,7 +3,7 @@
 # Define the path to the RPM package
 default_rpm_path="files/system/nordvpn*.rpm"
 
-# Function to install Cloudflare Warp
+# Function to install Nordvpn
 install_nordvpn() {
     local rpm_path=${1:-$default_rpm_path}
 
@@ -15,28 +15,3 @@ install_nordvpn() {
         exit 1
     fi
 }
-
-# Function to uninstall Cloudflare Warp
-uninstall_warp() {
-    echo "Uninstalling Nordvpn using rpm-ostree..."
-    if sudo rpm-ostree uninstall nordvpn; then
-        echo "Nordvpn uninstalled successfully."
-    else
-        echo "Failed to uninstall Norddvpn or it may not be installed."
-        exit 1
-    fi
-}
-
-# Main script logic
-case "$1" in
-    install-nordvpn)
-        install_nordvpn "$2"
-        ;;
-    uninstall-nordvpn)
-        uninstall_nordvpn
-        ;;
-    *)
-        echo "Usage: $0 {install-nordvpn [rpm_path] | uninstall-nordvpn}"
-        exit 1
-        ;;
-esac
