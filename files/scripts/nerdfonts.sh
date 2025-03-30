db1ce8e1e25df93dc61005c1b94d40cc9926da69
Faust Magno <neurochange@proton.me>
2025-03-30 20:47:21 +0100

    Update nerdfonts.sh

--- files/scripts/nerdfonts.sh
+++ files/scripts/nerdfonts.sh
@@ -6,4 +6,4 @@
 set -oue pipefail
 
 # Add Nerd Fonts
-curl -Lo /etc/yum.repos.d/_copr_che-nerd-fonts-"${FEDORA_MAJOR_VERSION}".repo https://copr.fedorainfracloud.org/coprs/che/nerd-fonts/repo/fedora-"${FEDORA_MAJOR_VERSION}"/che-nerd-fonts-fedora-"${FEDORA_MAJOR_VERSION}".repo
+curl -Lo /etc/yum.repos.d/_copr_che-nerd-fonts-"${FEDORA41}".repo https://copr.fedorainfracloud.org/coprs/che/nerd-fonts/repo/fedora-"${FEDORA41}"/che-nerd-fonts-fedora-"${FEDORA41}".repo
