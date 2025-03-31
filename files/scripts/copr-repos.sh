369fde47cac6bf7355ba6a2802e412e613c3c671
Faust Magno <neurochange@proton.me>
2025-03-31 16:05:58 +0100

    Delete files/scripts/copr-repos.sh

--- files/scripts/copr-repos.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-set -ouex pipefail
-
-# Add Nerd Fonts
-curl -Lo /etc/yum.repos.d/_copr_che-nerd-fonts-"${FEDORA_MAJOR_VERSION}".repo https://copr.fedorainfracloud.org/coprs/che/nerd-fonts/repo/fedora-"${FEDORA_MAJOR_VERSION}"/che-nerd-fonts-fedora-"${FEDORA_MAJOR_VERSION}".repo
