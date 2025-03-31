a9dcd1bfa9884c837f58323888096968c4953aa0
Faust Magno <neurochange@proton.me>
2025-03-31 14:18:03 +0100

    Delete files/scripts/nerdfonts.sh

--- files/scripts/nerdfonts.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell this script to exit if there are any errors.
-# You should have this in every custom script, to ensure that your completed
-# builds actually ran successfully without any errors!
-set -oue pipefail
-
-# Add Nerd Fonts
-curl -Lo /etc/yum.repos.d/_copr_che-nerd-fonts-"${fedora-$releasever-$basearch}".repo https://copr.fedorainfracloud.org/coprs/che/nerd-fonts/repo/fedora-"${fedora-$releasever-$basearch}"/che-nerd-fonts-fedora-"${fedora-$releasever-$basearch}".repo
