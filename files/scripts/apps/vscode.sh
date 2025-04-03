5dbced626e3f33f127156de0c6253de0d767a339
Faust Magno <neurochange@proton.me>
2025-04-03 13:19:52 +0100

    Delete files/scripts/apps directory

--- files/scripts/apps/vscode.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-set -ouex pipefail
-
-mkdir -p $HOME/AppImages
-
-wget "https://github.com/valicm/VSCode-AppImage/releases/download/latest/VSCode-x86_64.AppImage" -O ~/AppImages/VSCode-x86_64.AppImage
-
-chmod +x ~/AppImages/VSCode-x86_64.AppImage
