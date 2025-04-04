ae33bb800e78c7579bcefddef99e05b2491bc13b
Faust Magno <neurochange@proton.me>
2025-04-04 16:20:06 +0100

    Update vscode.sh

--- files/scripts/vscode.sh
+++ files/scripts/vscode.sh
@@ -2,6 +2,12 @@
 
 set -ouex pipefail
 
+# Ensure USER variable is set
+USER=${USER:-$(whoami)}
+
+# Your existing script content
+echo "Running as user: $USER"
+
 mkdir -p /var/home/$USER/AppImages
 
 wget "https://github.com/valicm/VSCode-AppImage/releases/download/latest/VSCode-x86_64.AppImage" -O /var/home/$USER/AppImages/VSCode-x86_64.AppImage
