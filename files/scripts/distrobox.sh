2b2af3bb3a8435a2e5d7b472e50570a3fce6d709
Faust Magno <neurochange@proton.me>
2025-04-04 20:52:09 +0100

    Update distrobox.sh

--- files/scripts/distrobox.sh
+++ files/scripts/distrobox.sh
@@ -9,4 +9,4 @@ distrobox create --image fedora:latest -n fedora -Y
 
 distrobox enter fedora
 
-sudo dnf install bat exa fd-find fzf rust cargo hugo go fortune-mod zeitfetch python3-i3ipc ripgrep thefuck zoxide pandoc poppler-devel poppler-utils ImageMagick jq p7zip p7zip-plugins tree exiftool btop xfce4-appearance-settings lxappearance fish && wget https://mega.nz/linux/repo/Fedora_41/x86_64/megasync-Fedora_41.x86_64.rpm && sudo dnf install "$PWD/megasync-Fedora_41.x86_64.rpm" && sudo dnf install https://prerelease.keybase.io/keybase_amd64.rpm
+sudo dnf install wget bat exa fd-find fzf rust cargo hugo go fortune-mod zeitfetch python3-i3ipc ripgrep thefuck zoxide pandoc poppler-devel poppler-utils ImageMagick jq p7zip p7zip-plugins tree exiftool btop xfce4-appearance-settings lxappearance fish && wget https://mega.nz/linux/repo/Fedora_41/x86_64/megasync-Fedora_41.x86_64.rpm && sudo dnf install "$PWD/megasync-Fedora_41.x86_64.rpm" && sudo dnf install https://prerelease.keybase.io/keybase_amd64.rpm
