b2749618cb69b0f455ce10b08123e4c400fc9297
Faust Magno <neurochange@proton.me>
2025-04-04 16:35:11 +0100

    Update icedrive.sh

--- files/scripts/icedrive.sh
+++ files/scripts/icedrive.sh
@@ -2,6 +2,16 @@
 
 set -ouex pipefail
 
+#!/bin/sh
+
+# Set default value for USER if not set
+: "${USER:=default_user}"
+
+# Add your script logic here
+echo "Running script as user: ${USER}"
+
+# Rest of the script
+
 mkdir -p /var/home/$USER/AppImages
 
 wget "https://cdn.icedrive.net/static/apps/linux/IcedriveMounted-v3.33-x86_64.AppImage" -O /var/home/$USER/AppImages/IcedriveMounted-v3.33-x86_64.AppImage
