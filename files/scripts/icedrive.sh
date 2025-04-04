9f106889ca372fc1e036be7dd8922440ffaedc90
Faust Magno <neurochange@proton.me>
2025-04-04 15:37:36 +0100

    Update icedrive.sh

--- files/scripts/icedrive.sh
+++ files/scripts/icedrive.sh
@@ -2,8 +2,8 @@
 
 set -ouex pipefail
 
-mkdir -p $HOME/AppImages
+mkdir -p /var/home/$USER/AppImages
 
-wget "https://cdn.icedrive.net/static/apps/linux/IcedriveMounted-v3.33-x86_64.AppImage" -O ~/AppImages/IcedriveMounted-v3.33-x86_64.AppImage
+wget "https://cdn.icedrive.net/static/apps/linux/IcedriveMounted-v3.33-x86_64.AppImage" -O /var/home/$USER/AppImages/IcedriveMounted-v3.33-x86_64.AppImage
 
-chmod +x ~/AppImages/IcedriveMounted-v3.33-x86_64.AppImage
+chmod +x /var/home/$USER/AppImages/IcedriveMounted-v3.33-x86_64.AppImage
