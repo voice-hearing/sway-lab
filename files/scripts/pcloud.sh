06db0132cee36ae8f552039338ed0238796ab77d
Faust Magno <neurochange@proton.me>
2025-04-04 16:33:47 +0100

    Update pcloud.sh

--- files/scripts/pcloud.sh
+++ files/scripts/pcloud.sh
@@ -2,6 +2,16 @@
 
 set -ouex pipefail
 
+#!/bin/sh
+
+# Set default value for USER if not set
+: "${USER:=default_user}"
+
+# Add your script logic here
+echo "Running script as user: ${USER}"
+
+# Rest of the script
+
 mkdir -p /var/home/$USER/AppImages
 
 wget "https://github.com/voice-hearing/sway-lab/blob/main/files/system/pcloud/pcloud.AppImage" -O /var/home/$USER/AppImages/pcloud.AppImage
