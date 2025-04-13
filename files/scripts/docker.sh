57c5f34055e27e0794fe20392fdc6177649e4917
Faust Magno <neurochange@proton.me>
2025-04-13 20:47:10 +0100

    Create docker.sh

---
+++ files/scripts/docker.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set ${SET_X:+-x} -eou pipefail
+
+if [[ ${IMAGE} =~ ucore ]]; then
+    $DNF remove -y docker-cli moby-engine
+fi
+
+# Setup repo
+cat <<EOF >/etc/yum.repos.d/docker-ce.repo
+[docker-ce-stable]
+name=Docker CE Stable
+baseurl=https://download.docker.com/linux/fedora/\$releasever/\$basearch/stable
+enabled=1
+gpgcheck=1
+gpgkey=https://download.docker.com/linux/fedora/gpg
+EOF
+
+$DNF install -y \
+    containerd.io \
+    docker-buildx-plugin \
+    docker-ce \
+    docker-ce-cli \
+    docker-compose-plugin
+
+# prefer to have docker-compose available for legacy muscle-memory
+ln -s /usr/libexec/docker/cli-plugins/docker-compose /usr/bin/docker-compose
+
+# Docker sysctl.d
+mkdir -p /usr/lib/sysctl.d
+echo "net.ipv4.ip_forward = 1" >/usr/lib/sysctl.d/docker-ce.conf
+
+# disable repo
+sed -i "s@enabled=1@enabled=0@" /etc/yum.repos.d/docker-ce.repo
