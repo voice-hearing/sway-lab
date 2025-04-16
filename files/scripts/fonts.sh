7bdc352994c709b3ddc87967d8f7c97461cb616f
Faust Magno <neurochange@proton.me>
2025-04-16 09:39:35 +0100

    Update fonts.sh

--- files/scripts/fonts.sh
+++ files/scripts/fonts.sh
@@ -11,3 +11,15 @@ curl -sfLo "$HOME/.local/share/fonts/Ubuntu Nerd Font Complete.ttf" https://gith
 cp -R files/fonts/ubuntu-font-family/*.ttf $HOME/.local/share/fonts/
 # Flush font cache
 fc-cache
+
+# GitHub Monaspace Font
+DOWNLOAD_URL=$(curl --retry 3 https://api.github.com/repos/githubnext/monaspace/releases/latest | jq -r '.assets[] | select(.name| test(".*.zip$")).browser_download_url')
+curl --retry 3 -Lo /tmp/monaspace-font.zip "$DOWNLOAD_URL"
+
+unzip -qo /tmp/monaspace-font.zip -d /tmp/monaspace-font
+mkdir -p /usr/share/fonts/monaspace
+mv /tmp/monaspace-font/monaspace-v*/fonts/variable/* /usr/share/fonts/monaspace/
+rm -rf /tmp/monaspace-font*
+
+fc-cache -f /usr/share/fonts/monaspace
+fc-cache --system-only --really-force --verbose
