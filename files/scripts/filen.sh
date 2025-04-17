287b7d8b1897c248918011f127f7400c8e8648af
Faust Magno <neurochange@proton.me>
2025-04-17 09:33:08 +0100

    Update filen.sh

--- files/scripts/filen.sh
+++ files/scripts/filen.sh
@@ -12,6 +12,8 @@ echo "Running script as user: ${USER}"
 
 mkdir -p /var/home/$USER/AppImages
 
-wget "https://github.com/FilenCloudDienste/filen-desktop/releases/download/v3.0.47/Filen_linux_x86_64.AppImage" -O /var/home/$USER/AppImages/Filen_linux_x86_64.AppImage
+wget --quiet --output-document /var/home/$USER/AppImages \
+
+"https://github.com/FilenCloudDienste/filen-desktop/releases/download/v3.0.47/Filen_linux_x86_64.AppImage"
 
 chmod +x /var/home/$USER/AppImages/Filen_linux_x86_64.AppImage
