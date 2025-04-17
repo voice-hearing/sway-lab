67f6c8722ddb4e9aefcccf2a67b391286b00fa5e
Faust Magno <neurochange@proton.me>
2025-04-17 09:24:01 +0100

    Update vscode.sh

--- files/scripts/vscode.sh
+++ files/scripts/vscode.sh
@@ -10,6 +10,8 @@ echo "Running as user: $USER"
 
 mkdir -p /var/home/$USER/AppImages
 
-wget "https://github.com/valicm/VSCode-AppImage/releases/download/latest/VSCode-x86_64.AppImage" -O /var/home/$USER/AppImages/VSCode-x86_64.AppImage
+wget --quiet --output-document /var/home/$USER/AppImages \
+
+"https://github.com/valicm/VSCode-AppImage/releases/download/latest/VSCode-x86_64.AppImage"
 
 chmod +x /var/home/$USER/AppImages/VSCode-x86_64.AppImage
